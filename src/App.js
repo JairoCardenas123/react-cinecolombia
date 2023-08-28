fa402f4e64a6443aab32781f41e337aa572697e1
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-08-28 13:36:48 -0500

    cinecolombia

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import buscadorIcono from './buscador.svg';
+import TarjetaPelicula from './TarjetaPelicula';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com?apiKey=1e6222ad';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const buscadorCine = async (titulo) => {
+    const response = await fetch(`${API_URL}&s=${titulo}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    buscadorCine('Spiderman');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Cine Colombia</h1>
+      <div className='busqueda'>
+        <input
+          placeholder="Buscador de Peliculas"
+          value='Spiderman'
+          onChange={() => {}}
+        />
+        <img
+          src={buscadorIcono}
+          alt='Buscar'
+          onClick={() => {}}
+        />
+      </div>
+
+        {movies?.length>0?(
+             <div className="container" >
+                {
+                    movies.map((movie)=>(
+                        <TarjetaPelicula movie1 = {movie}/>
+                    ))
+                }
+                  </div>
+        ):(
+            <div className='empty' >
+                <h2>No se encontraron Peliculas</h2>
+            </div>
+        )
+
+    }
+
+     
+       
+    
     </div>
   );
-}
+};
 
 export default App;
